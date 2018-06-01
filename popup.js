1969503f5ad6ce31c8d7d85aec583b8a6f658141
Sean Miller <sean.miller@hubdoc.com>
2018-06-01 17:51:52 -0400

    Popup almost done

--- popup.js
+++ popup.js
@@ -4,18 +4,18 @@
 // 2018-05-24
 
 
-let turnOnOrOff = document.getElementById('turnOnOrOff');
+let turnOnOrOff = document.getElementById('toggle');
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+// chrome.storage.sync.get('color', function(data) {
+//   changeColor.style.backgroundColor = data.color;
+//   changeColor.setAttribute('value', data.color);
+// });
 
-changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
-};
+// changeColor.onclick = function(element) {
+//     let color = element.target.value;
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//       chrome.tabs.executeScript(
+//           tabs[0].id,
+//           {code: 'document.body.style.backgroundColor = "' + color + '";'});
+//     });
+// };
